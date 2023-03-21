fd3aa06a43eb45cd566abd94ef51cd6300413b82
Стамо Петков <s.g.petkov@is-bg.net>
2023-03-21 22:12:36 +0200

    Add project files.

---
+++ Eventmi.Infrastructure/Data/EventmiDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Eventmi.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eventmi.Infrastructure.Data
+{
+    /// <summary>
+    /// Контекст, описващ базата данни
+    /// </summary>
+    public class EventmiDbContext : DbContext
+    {
+        /// <summary>
+        /// Създава контекст без настройки
+        /// </summary>
+        public EventmiDbContext()
+        {
+        }
+
+        /// <summary>
+        /// Създава контекст с предварителни настройки
+        /// </summary>
+        /// <param name="options">Настройки на контекста</param>
+        public EventmiDbContext(DbContextOptions<EventmiDbContext> options)
+            : base(options)
+        {
+        }
+
+        /// <summary>
+        /// Таблица със събития
+        /// </summary>
+        public DbSet<Event> Events { get; set; } = null!;
+    }
+}
